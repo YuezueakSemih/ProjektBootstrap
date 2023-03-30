fcc42e3ead2ef10807106249e41388c27585ee3e
SchmalzerTimon <t.schmalzer@students.htl-leonding.ac.at>
2023-03-30 11:26:25 +0200

    added a test .js and the project description

---
+++ Project Description.md
@@ -0,0 +1,1 @@
+# Project description: Hacker and Security
---
+++ js/Password guesser.js
@@ -0,0 +1,31 @@
+// Das geheime Passwort festlegen
+const secretPassword = "geheim123";
+
+// Die Anzahl der zulässigen Versuche festlegen
+const maxTries = 3;
+let currentTry = 0;
+
+// Funktion zur Überprüfung des eingegebenen Passworts
+function checkPassword() {
+  const input = document.getElementById("password-input").value;
+  currentTry++;
+
+  if (input.length < 8) {
+    alert("Das Passwort ist zu kurz!");
+  } else if (input.length > 20) {
+    alert("Das Passwort ist zu lang!");
+  } else if (input !== secretPassword) {
+    alert("Das Passwort ist inkorrekt! Versuchen Sie es erneut.");
+  } else {
+    alert("Herzlichen Glückwunsch, Sie haben das Passwort richtig geraten!");
+  }
+
+  if (currentTry === maxTries) {
+    alert("Sie haben alle Versuche aufgebraucht.");
+    document.getElementById("password-input").disabled = true;
+    document.getElementById("check-button").disabled = true;
+  }
+}
+
+// Ereignis-Listener zum Überprüfen des Passworts
+document.getElementById("check-button").addEventListener("click", checkPassword);
