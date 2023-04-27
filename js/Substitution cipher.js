82e06edd2d6cc2434b7c88ed49032d54a3c32e63
SchmalzerTimon <t.schmalzer@students.htl-leonding.ac.at>
2023-04-27 13:32:50 +0200

    added substitution cipher

--- js/Substitution cipher.js
+++ js/Substitution cipher.js
@@ -1,11 +1,12 @@
-// Define the Caesar cipher function
-let unsolvedWord = "GJgicKmlicwq";
-function caesarCipher(str, shift) {
+// Define the substitution cipher function
+function substitutionCipher(str, key) {
     str = str.toUpperCase();
+    key = key.toUpperCase();
     var result = "";
     for (var i = 0; i < str.length; i++) {
       var charCode = str.charCodeAt(i);
       if (charCode >= 65 && charCode <= 90) {
+        var shift = key.charCodeAt(charCode - 65) - 65;
         charCode = ((charCode - 65 + shift) % 26) + 65;
       }
       result += String.fromCharCode(charCode);
@@ -16,8 +17,8 @@ function caesarCipher(str, shift) {
   // Challenge logic
   function checkAnswer() {
     var answer = document.getElementById("answer").value.toUpperCase();
-    if (answer === caesarCipher(unsolvedWord, 2)) {
-      alert("Congratulations! You solved the Caesar Cipher Challenge.");
+    if (answer === substitutionCipher("HELLO WORLD", "ZYXWVUTSRQPONMLKJIHGFEDCBA")) {
+      alert("Congratulations! You solved the Substitution Cipher Challenge.");
     } else {
       alert("Sorry, that is not the correct answer. Please try again.");
     }
