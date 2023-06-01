a99f329411f8b920e89620de97e42241acc0545f
SchmalzerTimon <t.schmalzer@students.htl-leonding.ac.at>
2023-06-01 11:45:04 +0200

    added transposition cipher & changed caesar cipher

--- js/Caesar cipher.js
+++ js/Caesar cipher.js
@@ -1,47 +1,48 @@
 const unsolvedWord = "Oqpmga";
 const maxTries = 10;
 let currentTry = 0;
+
 // Define the Caesar cipher function
 function caesarCipher(str, shift) {
-    str = str.toUpperCase();
-    var result = "";
-    for (var i = 0; i < str.length; i++) {
-      var charCode = str.charCodeAt(i);
-      if (charCode >= 65 && charCode <= 90) {
-        charCode = ((charCode - 65 + shift) % 26) + 65;
-      }
-      result += String.fromCharCode(charCode);
+  str = str.toUpperCase();
+  var result = "";
+  for (var i = 0; i < str.length; i++) {
+    var charCode = str.charCodeAt(i);
+    if (charCode >= 65 && charCode <= 90) {
+      charCode = ((charCode - 65 + shift) % 26) + 65;
     }
-    return result;
+    result += String.fromCharCode(charCode);
   }
-  
-  // Challenge logic
-  function checkAnswer() {
-    var answer = document.getElementById("answer").value.toUpperCase();
-    let solvedWord = caesarCipher(unsolvedWord, 2);
-    if (answer === solvedWord) {
-      alert("Congratulations! You solved the Caesar Cipher Challenge.");
-    } 
-    else 
-    {
-      alert("Sorry, that is not the correct answer. Please try again.");
-      currentTry++;
-    }
+  return result;
+}
 
-    if(currentTry === (maxTries / 5))
-    {
-      alert("The first letter is " + solvedWord[0] + "You need to count the arrows!");
-    }
-    else if(currentTry === (maxTries / 2))
-    {
-      alert("The first 2 letters are " + solvedWord[0] + solvedWord[1] + "The word is moved by 2 letters!");
-    }
+// Function to display the result message
+function displayResult(message) {
+  var resultElement = document.getElementById('result');
+  resultElement.textContent = message;
+}
 
-    if(currentTry === maxTries)
-    {
-        alert("No tries left. Restart the page to try again.Here is a Video to learn about this algorythm: https://www.youtube.com/watch?v=l6jqKRXSShI");
-        document.getElementById("answer").disabled = true;
-        document.getElementById("check-button").disabled = true;
-    }
+// Challenge logic
+function checkAnswer() {
+  var answer = document.getElementById("answer").value.toUpperCase();
+  let solvedWord = caesarCipher(unsolvedWord, 2);
+
+  if (answer === solvedWord) {
+    displayResult('Congratulations! You solved the Caesar Cipher Challenge');
+  } else {
+    displayResult('Sorry, that is not the correct answer. Please try again.');
+    currentTry++;
   }
-  
+
+  if (currentTry === maxTries / 5) {
+    displayResult('The first letter is ' + solvedWord[0] + '. You need to count the arrows');
+  } else if (currentTry === maxTries / 2) {
+    displayResult('The first two letters are ' + solvedWord[0] + solvedWord[1] + '. The word is shifted by 2 letters');
+  }
+
+  if (currentTry === maxTries) {
+    displayResult('No tries left. Here is a quick video about the ceasar:  <a href="https://www.youtube.com/watch?v=l6jqKRXSShI" target="_blank">Video</a>.');
+    document.getElementById("answer").disabled = true;
+    document.getElementById("check-button").disabled = true;
+  }
+}
--- js/Transposition cipher.js
+++ js/Transposition cipher.js
@@ -1,2 +1,56 @@
-const maxTries = 10;
-let currentTry = 0;
+
+function transpositionCipherEncrypt(text, key) {
+    var encryptedText = '';
+    var numColumns = Math.ceil(text.length / key);
+  
+    for (var i = 0; i < key; i++) {
+      for (var j = 0; j < numColumns; j++) {
+        var index = i + j * key;
+        if (index < text.length) {
+          encryptedText += text.charAt(index);
+        }
+      }
+    }
+  
+    return encryptedText;
+  }
+  
+  // Funktion zum Starten des Ratespiels
+  function startGame() {
+    var secretText = 'HELLO WORLD'; // Geheimer Text
+    var key = 4; // Schlüssel für das Transpositions-Chiffre
+    var maxAttempts = 3; // Maximale Anzahl an Versuchen
+  
+    var encryptedText = transpositionCipherEncrypt(secretText, key);
+  
+    document.getElementById('welcome').innerHTML = 'Willkommen zum Ratespiel!';
+    document.getElementById('encryptedText').innerHTML = 'Der verschlüsselte Text lautet: ' + encryptedText;
+  
+    var attempts = 0;
+    var guessed = false;
+  
+    var guessInput = document.getElementById('guessInput');
+    var submitButton = document.getElementById('submitButton');
+    var resultMessage = document.getElementById('result');
+  
+    submitButton.addEventListener('click', function() {
+      var guess = guessInput.value;
+  
+      if (guess === secretText) {
+        resultMessage.innerHTML = 'Glückwunsch! Du hast den Text entschlüsselt.';
+        guessed = true;
+      } else {
+        attempts++;
+        if (attempts < maxAttempts) {
+          resultMessage.innerHTML = 'Leider falsch. Du hast noch ' + (maxAttempts - attempts) + ' Versuch(e) übrig.';
+        } else {
+          resultMessage.innerHTML = 'Du hast keine Versuche mehr. Der richtige Text lautet: ' + secretText;
+        }
+      }
+  
+      guessInput.value = ''; // Zurücksetzen des Eingabefelds
+      guessInput.focus(); // Fokus auf das Eingabefeld setzen
+    });
+  
+    guessInput.focus(); // Fokus auf das Eingabefeld setzen
+  }
